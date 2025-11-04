9dd12afcdbabbdffcbf6545bb71c7a61390ddfd3
takuto osugi <oosugi-t@vehicleivision.jp>
2025-11-05 01:25:31 +0900

    delete ts

--- wwwroot/ts/share/BackButton.ts
+++
@@ -1,9 +0,0 @@
-export class BackButton {
-    Buck = () => {
-        const element = document.getElementById('back') as HTMLButtonElement;
-
-        element.addEventListener("click", () => {
-            history.back();
-        });
-    }
-}
--- wwwroot/ts/share/ControlObject.ts
+++
@@ -1,9 +0,0 @@
-export class ControlObject {
-    static copyObject = (object: HTMLElement) => {
-        const cloneObject = object.cloneNode(true);
-        object.after(cloneObject);
-    }
-    static elaseObject = (object: HTMLElement) => {
-        object.remove();
-    }
-}
